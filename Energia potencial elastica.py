7c047b54e74b7020afbd37a9c200a7bf52adbbb5
Pedro Barbosa <156959341+Dilectus-a-Deo@users.noreply.github.com>
2024-05-29 17:53:28 -0300

    Add files via upload

---
+++ Energia potencial elastica.py
@@ -0,0 +1,12 @@
+#inserindo variaveis
+
+K = float(input('Digite o valor da constante: '))
+X = float(input('Digite o valor da distância: '))
+
+#Inserindo Formula
+
+E = K * X ** 2 / 2
+
+#Imprimindo o valor da energia
+
+print(f'Digite o valor : {E} J ')
